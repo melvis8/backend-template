2caa8bb0301e5ef0bdeec93cb615955aa0a493a0
Melvis8 <melvisnajela@gmail.com>
2025-04-24 15:27:48 +0100

    trying to resolve the issues with prismaclient in order to start testing my api with postman

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
+import userRoute from './routes/user.route';
+
+dotenv.config();
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.json());
+app.use('/api/users', userRoute(prisma));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ src/routes/user.route.ts
@@ -0,0 +1,62 @@
+import express from 'express';
+import {Request,Response} from 'express'
+
+export default function (prisma) {
+  const router = express.Router();
+
+  // Create
+  router.post('/', async (req: Request, res:Response) => {
+    const { name,age } = req.body;
+    try {
+      const user = await prisma.user.create({ data: { name } });
+      res.status(201).json(user);
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  });
+
+  // Read All
+  router.get('/', async (req: Request, res:Response) => {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  });
+
+  // Read One
+  router.get('/:id', async (req: Request, res:Response) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id: req.params.id },
+      });
+      if (!user) return res.status(404).json({ error: 'Not found' });
+      res.json(user);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
+
+  // Update
+  router.put('/:id', async (req: Request, res:Response) => {
+    const { name } = req.body;
+    try {
+      const user = await prisma.user.update({
+        where: { id: req.params.id },
+        data: { name },
+      });
+      res.json(user);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
+
+  // Delete
+  router.delete('/:id', async (req: Request, res:Response) => {
+    try {
+      await prisma.user.delete({ where: { id: req.params.id } });
+      res.json({ message: 'user deleted' });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
+
+  return router;
+}
